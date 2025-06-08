dfde7a8232247d2483fe9ba8071e72eb312c79c1
Vipul sharma <vipulsharma6021@gmail.com>
2025-06-08 16:47:20 +0530

    Remove unused GetX import from widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
 
 import 'package:gitaction/main.dart'; // Change this to match your app import
 
